78168482a39a63f0f368ba9601af887d66907d15
Hironao Namba <hironao.namba@gmail.com>
2017-01-05 00:30:47 +0900

    Send thanks letter via codenberg api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    def send_thanks_letter
+        client = CodenbergClient.new(Rails.application.secrets.codenberg_api_key, Rails.application.secrets.codenberg_secret)
+        client.create_order(Rails.application.secrets.codenberg_template_id,
+                            postal_code,
+                            prefecture,
+                            city,
+                            address,
+                            name,
+                            tel,
+                            [{id: Rails.application.secrets.codenberg_custom_field_id, value: name}]
+                            )
+    end
 end
